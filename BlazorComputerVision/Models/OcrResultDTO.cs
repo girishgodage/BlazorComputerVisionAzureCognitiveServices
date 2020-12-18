4b9e39c166f5048eceab5e79d7eb975187f53432
girish <girishgodage@gmail.com>
2020-12-18 14:33:25 +0530

    first commit

---
+++ BlazorComputerVision/Models/OcrResultDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorComputerVision.Models
+{
+    public class OcrResultDTO
+    {
+        public string Language { get; set; }
+        public string DetectedText { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Blazor-Computer-Vision-Azure-Cognitive-Services
+
+An Optical Character Recognition (OCR) using Angular and Azure Computer Vision Cognitive Services
+
+# Demo
+
+![ngComputerVision](https://github.com/girishgodage/BlazorComputerVisionAzureCognitiveServices/blob/master/Output/output.gif)
